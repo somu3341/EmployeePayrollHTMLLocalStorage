70712866340410af8ce510a05791f12f9f245bcf
somu3341 <somashekarvsv1996@gmail.com>
2023-05-04 21:10:07 +0530

    [somu]Add:Reset the Form On Clicking Reset.

--- Payroll-form.js
+++ Payroll-form.js
@@ -76,3 +76,17 @@ window.addEventListener('DOMContentLoaded',(event) => {
         let value = document.querySelector(id).value;
         return value;
     }
+    const unsetSelectedValues=(propertyValue)=>{
+        let allItems=document.querySelectorAll(propertyValue);
+        allItems.forEach(item=>{
+            item.checked =false;
+        })
+    }
+    const setTextValue=(id,value)=>{
+        const element = document.querySelector(id);
+        element.textContent=value;
+    }
+    const setValue=(id,value)=>{
+        const element = document.querySelector(id);
+        element.value=value;
+    }
